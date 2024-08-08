4c5dbf3cf9e55326b109a575e76f75de2d31cdcc
Michael Boyo <boyomichaelbidemi@gmail.com>
2024-08-08 07:37:29 +0100

    Functionals

--- functionals/Functionals.java
+++ functionals/Functionals.java
@@ -1,8 +1,6 @@
 package functionals;
 
-import java.util.function.BiFunction;
-import java.util.function.Consumer;
-import java.util.function.Function;
+import java.util.function.*;
 
 public class Functionals {
     public static void main(String[] args) {
@@ -14,9 +12,33 @@ public class Functionals {
         // a function is a functional interface that takes an argument and returns a result
         Function<Integer, Integer> square = a -> a * a;
         square.apply(38);
+
         // a consumer is a functional interface that takes an argument and returns nothing
         Consumer<Integer> consumer = a -> System.out.println(a);
         consumer.accept(10);
+
+        // a supplier is a functional interface that takes no arguments and returns a result
+        Supplier<Integer> supplier = () -> 10;
+        System.out.println(supplier.get());
+
+
+        // a predicate is a functional interface that takes an argument and returns a boolean
+        Predicate<Integer> isEven = i -> i % 2 == 0;
+
+        System.out.println(isEven.test(10));
+
+
+        // a unary operator is a functional interface that takes an argument and returns a result of the same type
+        UnaryOperator<Integer> increment = i -> i + 1;
+        System.out.println(increment.apply(10));
+
+
+
+        // a binary operator is a functional interface that takes two arguments and returns a result of the same type
+        // also, a binary operator is a specialization of a BiFunction
+        BinaryOperator<Integer> multiply = (a, b) -> a * b;
+        System.out.println(multiply.apply(2, 3));
+
 
     }
 
